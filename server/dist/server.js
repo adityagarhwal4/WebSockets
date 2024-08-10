0f5d1d79df7ff16f34f3bf6198c27415ca032207
adityagarhwal4 <adityagarhwal4@gmail.com>
2024-08-10 23:02:34 +0530

    compiled

--- server/dist/server.js
+++ server/dist/server.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log("hi");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+const app_1 = __importDefault(require("./app"));
+dotenv_1.default.config();
+const port = process.env.PORT;
+console.log("port testing: ", port);
+app_1.default.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
